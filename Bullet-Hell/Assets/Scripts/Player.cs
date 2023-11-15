7ddcb83366ee08f46a48677827de4741d2cddd9f
danfupo03 <danfupo@outlook.com>
2023-11-15 00:20:41 -0600

    feat(Player): Add player movement, with keyboard inputs

--- Bullet-Hell/Assets/Scripts/Player.cs
+++ Bullet-Hell/Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float speed = 30;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed = 5;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            speed = 30;
+        }
     }
 }
