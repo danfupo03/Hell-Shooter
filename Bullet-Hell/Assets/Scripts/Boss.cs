33f8dd1b05b5f8bca9da0162c4653591dcca1f2a
danfupo03 <danfupo@outlook.com>
2023-11-15 00:04:19 -0600

    feat(Boss): Add appearance animation

--- Bullet-Hell/Assets/Scripts/Boss.cs
+++ Bullet-Hell/Assets/Scripts/Boss.cs
@@ -10,22 +10,33 @@ public class Boss : MonoBehaviour
     [SerializeField]
     private float bulletSpeed;
 
-    // void Start()
-    // {
-    //     StartCoroutine(FireCircle());
-    //     StartCoroutine(FireSpiral(5));
-    //     StartCoroutine(FireFlower(5));
-    // }
-
     void Update()
     {
 
     }
 
+    public IEnumerator Appearance()
+    {
+        transform.position = new Vector3(-347, 45, 20);
+        Vector3 targetPos = new Vector3(-347, 13, 20);
+
+        Vector3 currentPos = transform.position;
+
+        float timeElapsed = 0;
+        float timeToMove = 3;
+
+        while (timeElapsed < timeToMove)
+        {
+            transform.position = Vector3.Lerp(currentPos, targetPos, timeElapsed / timeToMove);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
     IEnumerator FireCircle()
     {
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 20; i++)
         {
             for (int j = 0; j < 360; j += 10)
             {
@@ -37,7 +48,7 @@ public class Boss : MonoBehaviour
 
     IEnumerator FireSpiral()
     {
-        for (int i = 0; i < 360; i += 10)
+        for (int i = 0; i < 720; i += 10)
         {
             CreateBullet(Quaternion.Euler(0, 0, i) * Vector2.up);
             CreateBullet(Quaternion.Euler(0, 0, i + 90) * Vector2.up);
@@ -49,7 +60,7 @@ public class Boss : MonoBehaviour
 
     IEnumerator FireFlower()
     {
-        for (int i = 0; i < 360; i += 10)
+        for (int i = 0; i < 720; i += 10)
         {
             CreateBullet(Quaternion.Euler(0, 0, i) * Vector2.up);
             CreateBullet(Quaternion.Euler(0, 0, i + 90) * Vector2.up);
@@ -84,20 +95,25 @@ public class Boss : MonoBehaviour
 
     private void TimeCheck()
     {
-        switch (TimeManager.Minute)
+        if (TimeManager.Hour == 0)
         {
-            case 1:
-                StartCoroutine(FireCircle());
-                break;
-            case 10:
-                StartCoroutine(FireSpiral());
-                break;
-            case 20:
-                StartCoroutine(FireFlower());
-                break;
-            default:
-                break;
+            switch (TimeManager.Minute)
+            {
+                case 2:
+                    StartCoroutine(Appearance());
+                    break;
+                case 10:
+                    StartCoroutine(FireCircle());
+                    break;
+                case 31:
+                    StartCoroutine(FireSpiral());
+                    break;
+                case 46:
+                    StartCoroutine(FireFlower());
+                    break;
+                default:
+                    break;
+            }
         }
-
     }
 }
